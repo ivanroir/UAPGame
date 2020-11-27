e2dfa352ecd5bfc322da037d749a260cd35c418d
ivanroir <ivanroirbruselas75@gmail.com>
2020-11-27 08:58:23 +0800

    Update main.js

--- Photobooth/js/main.js
+++ Photobooth/js/main.js
@@ -264,11 +264,11 @@ function initCameraStream() {
       //context.drawImage(video, width * -1, -300, width, height * 1.32);
       if (screen.orientation.type == "portrait-primary")
       {
-        context.drawImage(video, (width * -1), 0, width * 1.25, height);
+        context.drawImage(video, (width * -1), 0, width, height);
       }
       else if (screen.orientation.type == "landscape-primary")
       {
-        context.drawImage(video, (width * -1), -170, width * 1.25, height * 1.25);
+        context.drawImage(video, (width * -1) * 40, -170, width * 1.20, height * 1.25);
       }
       context.restore();
     }
